b767280df2481efa01c6be6812023d30e3ad76af
Javier Ching <jhnching15@gmail.com>
2018-06-24 12:46:55 -0400

    ✨Simple TODO app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>My TODO App</h1>
+    <div>Todos remaining: <span :class="{green: todos.length === 0, red: todos.length > 0}">{{ todos.length }}</span></div>
+    <div>There are {{ unsavedTodos }} unsaved todos</div>
+    <ul>
+      <li v-for="todo in todos">
+        <todo-item :todo="todo" :saveTodo="saveTodo" :editTodo="editTodo" :deleteTodo="deleteTodo"></todo-item>
+      </li>
+    </ul>
+    <button @click="addTodo">Add todo</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoItem,
+  },
+  data: function() {
+    return {
+      todos: [],
+      incrementingId: 0,
+    }
+  },
+  computed: {
+    unsavedTodos: function() {
+      return this.todos.filter(todo => !todo.saved).length;
+    }
+  },
+  methods: {
+    findTodo: function(id) {
+      return this.todos.find(todo => todo.id === id)
+    },
+    addTodo: function() {
+      this.todos.push({
+        id: this.incrementingId++,
+        value: '',
+        saved: false,
+      });
+    },
+    saveTodo: function(id) {
+      const currentTodo = this.findTodo(id);
+      currentTodo.value ? currentTodo.saved = true : alert('Please enter a valid Todo');
+    },
+    editTodo: function(id) {
+      this.findTodo(id).saved = false;
+    },
+    deleteTodo: function(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="todo-item">
+  <span v-if="todo.value && todo.saved">
+    {{ todo.value }}
+    <button @click="editTodo(todo.id)">edit</button>
+  </span>
+  <span v-else>
+    <input v-model="todo.value"/>
+    <button @click="saveTodo(todo.id)">save</button>
+  </span>
+  <button @click="deleteTodo(todo.id)">delete</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['todo', 'saveTodo', 'editTodo', 'deleteTodo'],
+}
+</script>
