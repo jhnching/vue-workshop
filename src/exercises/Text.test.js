a79e7836dcfd0a888f6c788cc4f933f51c08d0f4
Javier Ching <jhnching15@gmail.com>
2018-07-05 16:01:11 -0400

    add exercises

--- src/exercises/Text.test.js
+++ src/exercises/Text.test.js
@@ -1,9 +1,65 @@
-import Vue from 'vue';
-import jsdom from 'jsdom';
-import TodoItem from '../app/components/TodoItem.vue';
+import { mount } from '@vue/test-utils';
+import Text from './Text';
 
 describe('Text', () => {
-  test('should work', () => {
-    expect(1).toEqual(1);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(Text);
+  });
+
+  test('should render the title', () => {
+    const expected = 'Hello World!';
+    const actual = wrapper.find('.hello-world h1').text();
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should render the computed message', () => {
+    const expected = 'euV ot noitcudortnI';
+    const actual = wrapper.find('.reverse-message').text();
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should apply the toUpperCase method on upperCaseMessage', () => {
+    const expected = 'SHOULD BE UPPERCASE TEXT';
+    const actual = wrapper.find('.to-uppercase').text();
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should apply styles', () => {
+    expect(wrapper.contains('#styled-red.red')).toBeTruthy();
+
+    wrapper.setData({ applyClass: false });
+
+    expect(wrapper.contains('#styled-red.red')).toBeFalsy();
+  });
+
+  test('should conditionally render with v-if', () => {
+    expect(wrapper.contains('#should-render')).toBeFalsy();
+
+    wrapper.setData({ shouldRender: true });
+
+    expect(wrapper.contains('#should-render')).toBeTruthy();
+  });
+
+  test('should render a list with v-for', () => {
+    expect(wrapper.find('ul').findAll('li').length).toEqual(3);
+  });
+
+  test('should increment the count when pressing on the button', () => {
+    expect(wrapper.vm.buttonClickCount).toEqual(0);
+
+    wrapper.find('button').trigger('click');
+    wrapper.find('button').trigger('click');
+
+    expect(wrapper.vm.buttonClickCount).toEqual(2);
+  });
+
+  test('should link input to inputText data key using v-model', () => {
+    wrapper.setData({ inputText: 'some text' });
+    expect(wrapper.find('input').element.value).toEqual('some text');
   });
 });
--- src/exercises/Text.vue
+++ src/exercises/Text.vue
@@ -1,13 +1,39 @@
 <template>
-    <div></div>
+    <div class="hello-world">
+        <h1><!-- PLACE MESSAGE HERE--></h1>
+        <p class="reverse-message"></p>
+        <p class="to-uppercase"></p>
+        <div id="styled-red"></div>
+        <div id="should-render">
+            <p>This will shown when shouldRender flag is true</p>
+        </div>
+        <ul></ul>
+        <button>click me!</button>
+        <input />
+    </div>
 </template>
 
 <script>
 export default {
   data: function() {
     return {
-      message: 'Hello World!',
+      title: 'Hello World!',
+      reversedMessage: 'Introduction to Vue',
+      upperCaseMessage: 'Should be uppercase text',
+      applyClass: true,
+      shouldRender: false,
+      listData: [1, 2, 3],
+      buttonClickCount: 0,
+      inputText: '',
     };
   },
+  computed: {},
+  methods: {},
 };
 </script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
