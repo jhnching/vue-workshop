2b52d89ad1d3fabe0aca46137e43fb4f1a07c86f
Javier Ching <jhnching15@gmail.com>
2018-07-29 14:52:11 -0400

    Add Vuex solutions

---
+++ src/vuex/store.js
@@ -0,0 +1,64 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const state = {
+  todos: [],
+  incrementingId: 0,
+};
+
+const getters = {
+  todos: state => state.todos,
+  unsavedTodos: state => state.todos.filter(todo => !todo.saved).length,
+};
+
+const mutations = {
+  addTodo: function(state) {
+    state.todos.push({
+      id: state.incrementingId++,
+      value: '',
+      saved: false,
+    });
+  },
+  saveTodo: function(state, payload) {
+    payload.todo.saved = true;
+  },
+  editTodo: function(state, payload) {
+    payload.todo.saved = false;
+  },
+  deleteTodo: function(state, payload) {
+    state.todos = state.todos.filter(todo => todo !== payload.todo);
+  },
+};
+
+const actions = {
+  addTodo(context) {
+    context.commit('addTodo');
+  },
+  saveTodo(context, todo) {
+    context.commit({
+      type: 'saveTodo',
+      todo,
+    });
+  },
+  editTodo(context, todo) {
+    context.commit({
+      type: 'editTodo',
+      todo,
+    });
+  },
+  deleteTodo(context, todo) {
+    context.commit({
+      type: 'deleteTodo',
+      todo,
+    });
+  },
+};
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions,
+});
