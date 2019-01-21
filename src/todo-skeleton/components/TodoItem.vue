db1e2947d0ed5ef1a88d9cf94520a0cd04dd29d1
Javier Ching <jhnching15@gmail.com>
2019-01-21 15:52:55 -0500

    ♻️ code cleanup

--- src/todo-skeleton/components/TodoItem.vue
+++ src/todo-skeleton/components/TodoItem.vue
@@ -1,26 +1,24 @@
 <template>
-  <div id="todo-item">
+  <div>
+    <!-- Display the value and edit button -->
+    <!-- Shown when completed editing a todo -->
+    <span>
+      <button>edit</button>
+    </span>
 
-  <!-- Display the value and edit button -->
-  <!-- Shown when completed editing a todo -->
-  <span>
-    <button>edit</button>
-  </span>
+    <!-- Allow editing with input and save button -->
+    <!-- Shown when editing a todo -->
+    <span>
+      <input />
+      <button>save</button>
+    </span>
 
-  <!-- Allow editing with input and save button -->
-  <!-- Shown when editing a todo -->
-  <span>
-    <input />
-    <button>save</button>
-  </span>
-
-  <button>delete</button>
-</div>
+    <button>delete</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'TodoItem',
   props: [],
 };
 </script>
