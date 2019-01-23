729e6c0abd736a9ed11d6781837e0b1a141a461a
Javier Ching <jhnching15@gmail.com>
2019-01-22 20:45:05 -0500

    formatting

--- src/exercises/Exercises.vue
+++ src/exercises/Exercises.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="hello-world">
-        <h1><!-- Place the title here --></h1>
+  <div class="hello-world">
+    <h1><!-- Place the title here --></h1>
 
-        <p class="reverse-message">
-            <!-- Convert toBeReversedMessage here with a computed function. Hint: Use the Array reverse method -->
-        </p>
+    <p class="reverse-message">
+        <!-- Convert toBeReversedMessage here with a computed function. Hint: Use the Array reverse method -->
+    </p>
 
-        <p class="to-uppercase"><!-- Convert toBeUpperCaseMessage here with a computed function --></p>
+    <p class="to-uppercase"><!-- Convert toBeUpperCaseMessage here with a computed function --></p>
 
-        <div id="styled-red"><!-- Apply the red class to this div based on the shouldApplyClass condition --></div>
+    <div id="styled-red"><!-- Apply the red class to this div based on the shouldApplyClass condition --></div>
 
-        <div id="should-render">
-            <p>This will show when shouldRender flag is true</p>
-        </div>
-
-        <ul><!-- Render a list from listData. --></ul>
-
-        <!-- Apply a click handler here to increment buttonClickCount per click -->
-        <button>click me!</button>
-
-        <!-- I will two way bind inputText data property! -->
-        <input />
+    <div id="should-render">
+        <p>This will show when shouldRender flag is true</p>
     </div>
+
+    <ul><!-- Render a list from listData. --></ul>
+
+    <!-- Apply a click handler here to increment buttonClickCount per click -->
+    <button>click me!</button>
+
+    <!-- I will two way bind inputText data property! -->
+    <input />
+  </div>
 </template>
 
 <script>
